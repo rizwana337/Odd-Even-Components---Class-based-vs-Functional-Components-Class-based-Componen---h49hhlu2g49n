8ea14f6792c438a2e3c4b4891b6c8ae289345040
rizwana337 <120568097+rizwana337@users.noreply.github.com>
2023-04-02 12:23:52 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from 'react';
 import '../styles/App.css';
 
 class Odd extends React.Component {
+  componentWillUnmount() {
+    console.log("Odd is unmounted");
+  }
 
   render() {
     return (
@@ -13,6 +16,9 @@ class Odd extends React.Component {
 }
 
 class Even extends React.Component {
+  componentWillUnmount() {
+    console.log("Even is unmounted");
+  }
 
   render() {
     return (
@@ -22,26 +28,75 @@ class Even extends React.Component {
     )
   }
 }
+
 class App extends React.Component {
-  handleChange(){
-    this.setState({even: !this.state.even})
-  }
   constructor(props){
     super(props)
     this.state = {even: true}
-    this.handleChange =  this.handleChange.bind(this)
+    this.handleChange = this.handleChange.bind(this)
   }
+
+  handleChange(){
+    this.setState({even: !this.state.even})
+  }
+
   render() {
     return (
       <div id="main">
         {this.state.even ? <Even /> : <Odd />}
-
         <button id="toggle" onClick={this.handleChange}>Change</button>
       </div>
     )
   }
-
 }
 
-
 export default App;
+
+
+// import React from 'react'
+// import '../styles/App.css';
+
+// class Odd extends React.Component {
+
+//   render() {
+//     return (
+//       <div id="odd">
+//         I am odd
+//       </div>
+//     )
+//   }
+// }
+
+// class Even extends React.Component {
+
+//   render() {
+//     return (
+//       <div id="even">
+//         I am even
+//       </div>
+//     )
+//   }
+// }
+// class App extends React.Component {
+//   handleChange(){
+//     this.setState({even: !this.state.even})
+//   }
+//   constructor(props){
+//     super(props)
+//     this.state = {even: true}
+//     this.handleChange =  this.handleChange.bind(this)
+//   }
+//   render() {
+//     return (
+//       <div id="main">
+//         {this.state.even ? <Even /> : <Odd />}
+
+//         <button id="toggle" onClick={this.handleChange}>Change</button>
+//       </div>
+//     )
+//   }
+
+// }
+
+
+// export default App;
